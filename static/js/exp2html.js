a7f45bd338a5e750aa578885b35a21beb4260c79
Anurag Yadav <yanurag994@gmail.com>
2024-05-03 02:37:34 -0400

    Updating Experience, Research and Industrial Certifications

--- static/js/exp2html.js
+++ static/js/exp2html.js
@@ -3,21 +3,24 @@ function readJSONFile(path, callback) {
     fetch(path)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error(`Network response was not ok`);
             }
             return response.json();
         })
         .then(data => callback(data))
-        .catch(error => console.error('Error fetching JSON file:', error));
+        .catch(error => console.error(`Error fetching JSON file:`, error));
 }
 
 // Function to generate HTML
 function generateHTML(data) {
-    let html = '';
+    let html = ``;
     for (const organization in data) {
         html += `<h3>${organization}</h3>`;
         data[organization].forEach(role => {
-            html += `<h4>${role.title}; ${role.location}</h4>`;
+            html += `<h5>${role.title} : ${role.location}`;
+            html +=  role.note ? `<span style="margin-left: 20px; color:black; font-size:12px">(${role.note})</span>` : ``;
+            html += `<span style="float:right;">${role.duration}</span>`;
+            html += `</h5>`;
             if (role.achievements) {
                 html += "<ul>";
                 role.achievements.forEach(achievement => {
@@ -37,8 +40,8 @@ function generateHTML(data) {
     return html;
 }
 
-readJSONFile('deps/experience.json', function (data) {
+readJSONFile(`deps/experience.json`, function (data) {
     const html = generateHTML(data);
     // Do something with the generated HTML, like appending it to the DOM
     document.getElementById("Experience").innerHTML = html;
-  });
+});
